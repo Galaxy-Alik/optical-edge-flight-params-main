6d778110ce1d43751dd20a029e6fc544eeed9a85
unknown <aliktrikemaf@gmail.com>
2024-06-06 15:36:44 +0530

    Add BA_v3 + cmds

--- py_files/Utils.py
+++ py_files/Utils.py
@@ -16,6 +16,7 @@ import sys
 from pymap3d import geodetic2enu, enu2geodetic
 import warnings
 warnings.filterwarnings('ignore')
+from vo import FeatureLocalization
 
 def jetson_timestamp_to_utc(jetson_timestamp):
 
@@ -30,7 +31,7 @@ def jetson_timestamp_to_utc(jetson_timestamp):
 
     return utc_time_str
 
-def closest_ea(target_timestamp, rev_flight_obj, df_data, want_prev = False):
+def closest_ea(target_timestamp, rev_flight_obj, df_data, want_timestamp = False, want_prev = False):
 
     # Calculate the absolute difference between the target timestamp and each timestamp in the list
     utc_target_timestamp = rev_flight_obj[target_timestamp]
@@ -53,11 +54,15 @@ def closest_ea(target_timestamp, rev_flight_obj, df_data, want_prev = False):
       sampled_row = df_ins_data_all.loc[df_ins_data_all[p_col] == packet_count]
 
     closest_euler_angle = [list(sampled_row[' Roll_E'])[0], list(sampled_row[' Pitch_N'])[0], list(sampled_row[' Yaw_U'])[0]]
+    
+    if want_timestamp:
+      return closest_euler_angle, closest_timestamp 
+    
+    else:
+      return closest_euler_angle
 
-    return closest_euler_angle
 
-
-def closest_t(target_timestamp, rev_flight_obj, df_data, want_prev = False):
+def closest_t(target_timestamp, rev_flight_obj, df_data, want_timestamp = False, want_prev = False):
 
     utc_target_timestamp = rev_flight_obj[target_timestamp]
     dt = datetime.datetime.strptime(utc_target_timestamp, "%Y-%m-%d %H:%M:%S.%f")
@@ -80,7 +85,11 @@ def closest_t(target_timestamp, rev_flight_obj, df_data, want_prev = False):
 
     closest_dir = [list(sampled_row[' Latitude'])[0], list(sampled_row[' Longitude'])[0], list(sampled_row[' AltEllipsoid'])[0]]
 
-    return closest_dir
+    if want_timestamp:
+      return closest_dir, closest_timestamp 
+    
+    else:
+      return closest_dir
 
 def get_unix_timestamps(jetson_timestamp):
 
@@ -133,10 +142,9 @@ def trans_vec_to_lla(trans_vec, R, scale_factor, initial_ea, initial_coordinates
 
 class EAE():
 
-  def __init__(self, first_frame, second_frame, FeatureLocalization):
-    self.FeatureLocalization = FeatureLocalization
-    self.img1 = first_frame
-    self.img2 = second_frame
+  def __init__(self, second_frame, first_frame):
+    self.img1 = second_frame
+    self.img2 = first_frame
 
   def calc_euler_angle(self, rot_matrix):
 
@@ -161,7 +169,7 @@ class EAE():
     return roll, pitch, yaw
 
   def run(self, is_store = False):
-    feat = self.FeatureLocalization(self.img1, self.img2)
+    feat = FeatureLocalization(self.img1, self.img2)
     baseImage_kp, baseImage_des, secImage_kp, secImage_des = feat.feature_detection()
     matches = feat.feature_match(baseImage_des, secImage_des)
     rot_matrix, t_vec = feat.estimatePose(matches, baseImage_kp, secImage_kp)
@@ -192,3 +200,100 @@ class EAE():
       return euler_angle_rad, euler_angle_deg
 
 
+def findHomography(matches, kp1, kp2):
+    
+    if len(matches) < 4:
+        print("Not enough matches found between the images to compute homography")
+        exit(0)
+
+    baseImage_pts = []
+    secImage_pts = []
+    for match in matches:
+        baseImage_pts.append(kp1[match[0].queryIdx].pt)
+        secImage_pts.append(kp2[match[0].trainIdx].pt)
+
+    baseImage_pts = np.float32(baseImage_pts)
+    secImage_pts = np.float32(secImage_pts)
+
+    (homographyMatrix, status) = cv2.findHomography(secImage_pts, baseImage_pts, cv2.RANSAC, 4.0)
+    return homographyMatrix, status
+
+def getNewFrameSizeAndMatrix(homographymatrix, shape_img2, shape_img1):
+
+    (h, w) = shape_img2
+
+    initialmatrix = np.array([[0, w - 1, w - 1, 0],
+                          [0, 0, h - 1, h - 1],
+                          [1, 1, 1, 1]])
+
+    finalmatrix = np.dot(homographymatrix, initialmatrix)
+    [x, y, c] = finalmatrix
+    x = np.divide(x, c)
+    y = np.divide(y, c)
+
+    min_x, max_x = int(round(min(x))), int(round(max(x)))
+    min_y, max_y = int(round(min(y))), int(round(max(y)))
+
+    modW, modH = max_x, max_y
+    correction = [0,0]
+    if min_x < 0:
+        modW -= min_x
+        correction[0] = abs(min_x)
+
+    if min_y < 0:
+        modH -= min_y
+        correction[1] = abs(min_y)
+
+    if modW < shape_img1[1] + correction[0]:
+        modW = shape_img1[1] + correction[0]
+    if modH < shape_img1[0] + correction[1]:
+        modH = shape_img1[0] + correction[1]
+
+    x = np.add(x, correction[0])
+    y = np.add(y, correction[1])
+    
+    initialPts= np.float32([[0, 0],
+                                [w - 1, 0],
+                                [w - 1, h - 1],
+                                [0, h - 1]])
+    finalPts = np.float32(np.array([x, y]).transpose())
+    homographyMatrix = cv2.getPerspectiveTransform(initialPts, finalPts)  
+    return [modH, modW], correction, homographyMatrix
+
+def compute_homography(img1, img2):
+
+    feat = FeatureLocalization(img1, img2)
+    baseImage_kp, baseImage_des, secImage_kp, secImage_des = feat.feature_detection()
+    matches = feat.feature_match(baseImage_des, secImage_des)
+    homography_matrix, _ = findHomography(matches, baseImage_kp, secImage_kp)
+
+    return homography_matrix
+
+def stitch_frames(img1, img2, curr_R, curr_t):
+
+    homographyMatrix = compute_homography(img1, img2)
+    newFrameSize, correction, homographyMatrix = getNewFrameSizeAndMatrix(homographyMatrix, img2.shape[:2], img1.shape[:2])
+    stitchedFrame = cv2.warpPerspective(img2, homographyMatrix, (newFrameSize[1], newFrameSize[0]))
+    stitchedFrame[correction[1]:correction[1] + img1.shape[0], correction[0]:correction[0] + img1.shape[1]] = img1
+
+    return stitchedFrame
+
+def euler_angles_to_rotation_matrix(ea):
+
+    roll, pitch, yaw = ea
+
+    Rx = np.array([[1, 0, 0],
+                   [0, np.cos(roll), -np.sin(roll)],
+                   [0, np.sin(roll), np.cos(roll)]])
+
+    Ry = np.array([[np.cos(pitch), 0, np.sin(pitch)],
+                   [0, 1, 0],
+                   [-np.sin(pitch), 0, np.cos(pitch)]])
+
+    Rz = np.array([[np.cos(yaw), -np.sin(yaw), 0],
+                   [np.sin(yaw), np.cos(yaw), 0],
+                   [0, 0, 1]])
+
+    rotation_matrix = Rz @ Ry @ Rx
+
+    return rotation_matrix
